d9b020cf709de6fa0c52ba1420cfb6c25364b1be
pandaec <ericchan1026@gmail.com>
2023-08-05 14:44:27 +0800

    02 done

--- 02-means-to-end/main_test.go
+++ 02-means-to-end/main_test.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
 	"testing"
 )
 
-func TestIsPrime(t *testing.T) {
-	// hex1 := "51"
-	s := "49"
-	data, _ := hex.DecodeString(s)
-	fmt.Printf("% x", data)
+func TestDummy(t *testing.T) {
 }
